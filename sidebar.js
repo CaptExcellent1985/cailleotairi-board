587c9007ff79e7da16f58574e28f0fd8bfe65ba4
CaptExcellent1985 <joshuahorney1986@gmail.com>
2026-06-30 13:27:58 -0400

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -3,23 +3,44 @@ document.addEventListener("DOMContentLoaded", function() {
     
     if (!sidebarContainer) return;
 
-    // 1. Define your sidebar HTML structure here ONE time
+    // 1. Injected HTML layout - Notice "collapsed" is added directly to the category headers now
     sidebarContainer.innerHTML = `
-        <div class="index-category" onclick="toggleIndexCategory(this)">Phenomena & Arcana</div>
+	
+		<div class="index-category collapsed" onClick="toggleIndexCategory(this)">Cartography</div>
+		<ul class="index-menu-list">
+			<li><a href="../../map.html" id="link-Cartography-Tir">The Tir</a></li>
+		</ul>
+		</div>
+	
+        <div class="index-category collapsed" onclick="toggleIndexCategory(this)">The Tuatha</div>
         <ul class="index-menu-list">
-            <li><a href="anord-pulses.html" id="link-anord-pulses">🌀 The Anord Pulses</a></li>
-            <li><a href="confluence-geometry.html" id="link-confluence-geometry">📐 Confluence Geometry</a></li>
-            <li><a href="wild-magic-pockets.html" id="link-wild-magic-pockets">⚡ Wild Magic Containment</a></li>
+            <li><a href="Eolian Archives/Tribes/Dwarves.html" id="link-tribes-dwarves">⚒️Dwarves</a></li>
+            <li><a href="" id="link-tribes-Eladrin">🎭 Eladrin</a></li>
+            <li><a href="" id="link-tribes-Feyfolk">✨ Feyfolk</a></li>
+			<li><a href="" id="link-tribes-Firbolg">🌲 Firbolg </a></li>
+			<li><a href="" id="link-tribes-Gnomes">🔧 Gnomes </a></li>
+			<li><a href="" id="link-tribes-Goblins">🦴 Goblins </a></li>
+			<li><a href="" id="link-tribes-Goliath">🏔️ Goliath </a></li>
+			<li><a href="" id="link-tribes-Halfling">🏡 Halflings </a></li>
+			<li><a href="" id="link-tribes-Kenku">🐦 Kenku </a></li>
+			<li><a href="" id="link-tribes-Kobolds">⛏️ Kobolds</a></li>
+			<li><a href="" id="link-tribes-Lizard-folk">🦎 Lizard-folk </a></li>
+			<li><a href="" id="link-tribes-Tabaxi">🐾 Tabaxi </a></li>
+			<li><a href="" id="link-tribes-Tieflings">⛓️ Tieflings </a></li>
+			<li><a href="" id="link-tribes-Tritons">🔱 Tritons </a></li>
+			<li><a href="" id="link-tribes-TwilightElves">🌙 Twilight Elves			</a></li>
+			<li><a href="" id="link-tribes-WoodElves">🍃 Wood Elves </a></li>
         </ul>
+		</div>
 
-        <div class="index-category" onclick="toggleIndexCategory(this)">The Tribal Accord</div>
+        <div class="index-category collapsed" onclick="toggleIndexCategory(this)">The Tribal Accord</div>
         <ul class="index-menu-list">
             <li><a href="factions-beaver-tribe.html" id="link-factions-beaver-tribe">🦫 The Beaver-Tribe</a></li>
             <li><a href="factions-moon-weavers.html" id="link-factions-moon-weavers">🌙 The Moon-Weaver Coven</a></li>
             <li><a href="factions-rust-guard.html" id="link-factions-rust-guard">🛡️ The Iron & Rust Guard</a></li>
         </ul>
 
-        <div class="index-category" onclick="toggleIndexCategory(this)">Hazard Catalog</div>
+        <div class="index-category collapsed" onclick="toggleIndexCategory(this)">Hazard Catalog</div>
         <ul class="index-menu-list">
             <li><a href="flora-fauna-hazards.html" id="link-flora-fauna-hazards">🌿 Hyper-Aggressive Flora</a></li>
             <li><a href="monsters-warped-beasts.html" id="link-monsters-warped-beasts">🦌 Anord-Warped Beasts</a></li>
@@ -34,10 +55,17 @@ document.addEventListener("DOMContentLoaded", function() {
     
     if (activeLink) {
         activeLink.classList.add("active");
+        
+        // 🚀 SMART ADDITION: If a page inside a category is open, auto-expand that category!
+        const parentList = activeLink.closest('.index-menu-list');
+        const siblingHeader = parentList.previousElementSibling;
+        if (siblingHeader && siblingHeader.classList.contains('index-category')) {
+            siblingHeader.classList.remove('collapsed');
+        }
     }
 });
 
-// The toggle function remains available globally for the click event
+// Improved toggle engine that works seamlessly with your CSS rules
 function toggleIndexCategory(header) {
     header.classList.toggle('collapsed');
 }
