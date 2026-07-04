714300854ce0d1bc77d5558245c7a776a1246842
CaptExcellent1985 <joshuahorney1986@gmail.com>
2026-07-04 17:08:12 -0400

    Add files via upload

--- items-data.js
+++ items-data.js
@@ -1,5 +1,11 @@
 // External database of magic shop items
 const ITEM_DATABASE = {
+	"blank-blank": {
+    name: "Blank-Copy-Paste",
+    description: "Copy and Paste for New Items",
+	image: "Insert Image Path Here.ext"
+	},
+	
     "everlasting-oil-can": {
         name: "Everlasting Oil Can",
         description: "This ornately carved oil can, made from a polished elderwood and a hollowed mountain goat horn as a spout, holds an ever-refilling reservoir of magical oil. One application of this oil to any axe or saw provides a full day's worth of smooth operation, reducing the risk of jams and increasing cutting efficiency. It never needs refilling, but loses its magic if damaged beyond repair.",
@@ -7,6 +13,26 @@ const ITEM_DATABASE = {
 	},
     "talking-timber-tongs": {
         name: "Talking Timber Tongs",
-        description: "Heavy iron tongs used to lift lumber. When clamped onto a log, it loudly announces the structural stability, moisture content, and density of the wood in a monotone voice."
-    }
+        description: "Heavy iron tongs used to lift lumber. When clamped onto a log, it loudly announces the structural stability, moisture content, and density of the wood in a monotone voice.",
+		image:"images/items/TalkingTinkerTongs.png"
+	},
+	"whistling-wedge": {
+    name: "Whistling Wedge",
+    description: "This seemingly ordinary wedge formed from black quartz has a small whistle embedded in its side. When driven into a tree trunk and the whistle is blown, the wedge vibrates with a magical thrum, causing loose bark and small branches to fall away from the cutting zone, saving time and effort for the lumberjack.",
+	image: "images/items/WhistlingWedge.png"
+	},	
+	"elven-swift-saws": {
+    name: "Elven Swift Saws",
+    description: "These lightweight saws, crafted from dragonwood and petrified bone shards and imbued with a touch of fey magic, are incredibly sharp and light.",
+	image: "images/items/ElvenSwiftSaws.png"
+	},	
+	"compass-of-falling-giants": {
+    name: "Compass of Falling Giants",
+    description: "This intricate compass, housed in a polished hardwood case, points not to magnetic north, but to the nearest location where a large tree has recently fallen.",
+	image: "images/items/CompassOfFallingGiants.png"
+	},	
+	"twigs-prototypes": {
+    name: "Twig's Prototypes",
+    description: "A grab bag of un-tested items created by TWig Tinkerer. USE AT OWN RISK"
+	}
 };
